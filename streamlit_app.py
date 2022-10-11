3d179aa7b4d4c2168badd9255aad8cfd39f7da48
raulbarba1997 <115575630+raulbarba1997@users.noreply.github.com>
2022-10-11 18:37:22 +0200

    Update streamlit_app.py
    pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 
 import streamlit
+import pandas
+
 streamlit.title("My parents new healthu diner")
 streamlit.header('Menú de desayuno')
 streamlit.text('Omega 3 y avena con arándanos')
 streamlit.text('Batido de col rizada, espinacas y rúcula')
 streamlit.text('Huevo de gallinas camperas hervidas')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
