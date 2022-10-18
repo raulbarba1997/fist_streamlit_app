d3c56a6993ba0977c13e064d2248bd033fe8ddb0
raulbarba1997 <115575630+raulbarba1997@users.noreply.github.com>
2022-10-18 16:32:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,5 @@ streamlit.dataframe(my_data_rows)
 #añadimos un cuadro de texto para filtrar
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit'))
